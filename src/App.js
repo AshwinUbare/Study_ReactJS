dd08f01097a43eafea257ab44cfe14ad33f2a54f
AshwinKumar Narendra Ubare <ashwinubare@gmail.com>
2024-01-26 20:17:59 +0530

    2stCommit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   
   // let name1 ="Neha"; // local variable
 
-   let [name1, setName1]= useState("Priti")
+   let [name1, setName1]= useState("Priti");
 
    function changeData()
    {
@@ -25,9 +25,19 @@ function App() {
       setName1("Neha");
    }
 
-   let {name, setName}= useState();
+   let [name2, setName2]= useState();
+
+   let [age, setAge]= useState();
+
+   let countries = [
+       {name: "India",capital:"New Delhi"},
+       {name: "USA",captial:"Washington D.C."},
+       {name: "South Frica",capital:"Cape Town"},
+       {name: "New zealand",captial:"Wellington"}
+   ]
 
   return (
+
     <div className="App">
       
       <h1>Welcome in ReactJs</h1>
@@ -46,13 +56,39 @@ function App() {
 
        
 
-       <h3>Name : </h3>
+       <h3>Name : </h3> 
+       {/* <h3>{name2} {age}</h3> */}
+
        <input type='text' placeholder='Enter Name' onChange={(event) => {
-         console.log(event.target.value) 
+         setName2(event.target.value) 
+         
          // event = info of event & element where element is gating 
          // target = it refer to the element on which the event was triggered 
-        
         }}/>
+
+        <h3>Age :</h3>
+        <input type='number' placeholder='Enter Age' onChange={(event)=>{
+          setAge(event.target.value)
+        }}/>
+
+        <button type='button' onClick={()=> {
+          console.log(name2,age)
+          }}>Submit</button>
+
+         
+         <div className='data'>
+          <h3>{countries[0].name}</h3>
+          <p>{countries[0].capital}</p>
+         </div>
+         
+         {
+               countries.map((country)=>{})       
+                 return
+              })
+         }
+
+
+        
     </div>
     
   );
